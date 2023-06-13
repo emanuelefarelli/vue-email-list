52c6d222d23d8ab6d0be2758406eb43d93cb773e
emanuelefarelli <emaaliens888@gmail.com>
2023-06-13 16:50:49 +0200

    Printed on dom 10 random email

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ createApp({
     },
 
     created(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then( (response) => {
-            console.log('ok');
-        });
+        for(let i=0;i<10;i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                console.log('ok');
+                const email = response.data;
+                this.emailArray.push(email.response);
+            });
+        };
     }
 }).mount('#app');
